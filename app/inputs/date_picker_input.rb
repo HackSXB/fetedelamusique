8dbb2138a13c76f77e51f03068e87cdde7431dd3
Soulou <leo@unbekandt.eu>
2013-05-31 23:27:05 +0200

    Fix datepicker and coffee issue

--- app/inputs/date_picker_input.rb
+++
@@ -1,7 +0,0 @@
-class DatePickerInput < BasePicker
-  def input
-    @picker_name = 'date_picker'
-    @picker_format = "yyyy-MM-dd"
-    super
-  end
-end
--- app/inputs/datetime_picker_input.rb
+++
@@ -1,7 +0,0 @@
-class DatetimePickerInput < BasePicker
-  def input
-    @picker_name = 'datetime_picker'
-    @picker_format = "yyyy-MM-dd hh:mm"
-    super
-  end
-end
