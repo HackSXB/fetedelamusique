5ed77c929c36def77fa942e92e29e32855cfceea
Yann Klis <yann.klis@novelys.com>
2013-05-31 19:49:18 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fetedelamusique::Application.config.secret_token = 'd8345abbd80fd1208e225d33db213f2e5f9ca0ebb7230ff4aff8af979156ac8311804727449c56891835dd6540b2a7827d500102057aac1fcdff026dc591ef08'
